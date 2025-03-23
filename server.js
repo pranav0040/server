22093fc702498fcad69b44000e86e7fea8d5b04d
Pranav Maithani <150612315+pranav0040@users.noreply.github.com>
2025-03-23 19:58:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT=process.env.PORT || 4000
  
 const app=express()
 app.use(express.json())
-app.use(cors({ origin: `https://text-to-image-pranav.netlify.app/`, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 
 await connectDB()
 
